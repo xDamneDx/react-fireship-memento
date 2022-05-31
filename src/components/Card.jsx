1266fe7ef9b4053b5cab462f63e25a8edf76d2f3
xDamneDx <xdamnedx@gmail.com>
2022-05-31 17:43:06 +0300

    Created Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+export default function Card({ image, selected, onClick }) {
+  return (
+    <div className="card">
+      <div className={selected && "selected"}>
+        <img src={image} alt="" className="card-face" />
+        <img src={"/assets/fireship.png"} alt="" className="card-back" />
+      </div>
+    </div>
+  );
+}
