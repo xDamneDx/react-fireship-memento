0018f13dbd4589e4f853a5404bb35ad31964d385
xDamneDx <xdamnedx@gmail.com>
2022-05-31 17:46:39 +0300

    Added Card component & shuffled array w/ cards

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import shuffle from "./utilities/shuffle";
+
+// Components:
+import Card from "./components/Card";
+
+function App() {
+  const [cards, setCards] = useState(shuffle);
+
+  return (
+    <>
+      <div className="grid">
+        {cards.map((card) => {
+          const { image, id, matched } = card;
+
+          return (
+            <Card key={id} image={image} selected={false} onClick={() => {}} />
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
