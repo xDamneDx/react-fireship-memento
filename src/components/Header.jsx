fb24bc40ce7d92b8ddfbf7ad98466c18268b5eb1
xDamneDx <xdamnedx@gmail.com>
2022-06-01 12:03:08 +0300

    Created Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+
+export default function Header({ handleNewGame, wins }) {
+  // Update page title with win count:
+  useEffect(() => {
+    document.title = `${wins} wins`;
+  }, [wins]);
+
+  return (
+    <header className="header">
+      <h4>{wins} wins</h4>
+      <h3>Memory Game</h3>
+      <button onClick={handleNewGame}>New Game</button>
+    </header>
+  );
+}
